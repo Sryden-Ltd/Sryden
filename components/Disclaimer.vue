33e67ba491991c1dc80ca7e2a0164e3262b4fd29
toki0179 <olliwes01@gmail.com>
2022-06-20 15:58:51 +0100

    Feat: Added communication disclaimer

---
+++ components/Disclaimer.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <div class="flex flex-col min-h-screen">
+        <div class="pt-6">
+            <div class="text-center">
+                <h1 class="text-4xl tracking-wide text-white text-gray-800 font-bold">Communications Disclaimer</h1>
+            </div>
+            <div class="container px-6 py-4 mx-auto">
+        <div class="flex flex-col w-full">
+            <div>
+                <p class="text-xl pb-2 font-bold mt-4 text-gray-300 text-gray-600">The content of this email is confidential and intended for the recipient specified in message only. It is strictly forbidden to share any part of this message with any third party, without a written consent of the sender. If you received this message by mistake, please reply to this message and follow with its deletion, so that we can ensure such a mistake does not occur in the future.</p>
+                <p class="text-lg">
+
+                </p>
+            </div>
+        </div>
+    </div>
+    </div>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Disclaimer'
+}
+</script>
---
+++ pages/legal/communication-disclaimer.vue
@@ -0,0 +1,37 @@
+<template>
+  <body>
+      <navbar />
+        <Disclaimer />
+      <Footer />
+  </body>
+</template>
+
+<script>
+
+export default {
+    name: "Communication-DisclaimerPage",
+    data() {
+      return {
+        title: "Communication Disclaimer - Sryden",
+      }
+    },
+    head() {
+      return {
+        title: this.title,
+        meta: [
+          {
+            hid: "description",
+            name: "description",
+            content: "Sryden is an independent development studio based out of Scotland, creating software solutions for one of the UK's largest industrial sectors: Web hosting companies.",
+          },
+        ],
+      }
+    }
+}
+</script>
+
+<style>
+  html {
+    scroll-behavior: smooth;
+  }
+</style>
